cfd8fc48b43f41324f48d0b972356f48ecced6b8
rnader <rodrigonader3@gmail.com>
2022-03-25 00:50:43 -0300

    se agregaron botones de redes sociales y el facebook y instagram al footer, se le tiene que agregar el url
    Signed-off-by: rnader <rodrigonader3@gmail.com>

--- source/NumenProyect/src/components/Instagram/TellAFriend.jsx
+++ source/NumenProyect/src/components/Instagram/TellAFriend.jsx
@@ -1,8 +1,16 @@
 import React, { Fragment } from 'react'
 import { Box } from '@mui/material'
-
+import { Typography } from '@mui/material'
+import { Button } from '@mui/material'
+import { Grid } from '@mui/material'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
 const TellAFriend = () => {
+  
   return (
+
     <Fragment>
       <Box
         sx={{
@@ -12,7 +20,62 @@ const TellAFriend = () => {
           justifyContent: 'center',
         }}
       >
-        TellAFriend
+        <Typography style={{fontSize:"2rem",}}>
+          Contacto
+        </Typography>
+        <div>
+        </div>
+      </Box>
+
+      <Box
+          sx={{
+          margin:'10px',
+          display: 'flex',
+          justifyContent: 'space-around',
+          }}
+      >
+
+          <Button
+          href="https://www.facebook.com"
+          color="primary" 
+          variant="contained"
+          style={{padding: "2rem",}}>
+          <Box>
+            <FacebookIcon fontSize='large'/>
+          </Box> 
+          
+          </Button>
+          <Button
+          href="https://www.instagram.com/?hl=es-la"  
+          color="secondary" 
+           variant="contained"
+           style={{padding: "2rem",}}>
+            <Box>
+              <InstagramIcon fontSize='large'/>
+           </Box> 
+          
+          </Button>
+          <Button
+           href="https://twitter.com/home" 
+           color="info"
+           variant="contained"
+           style={{padding: "2rem",}}>
+             <Box>
+               <TwitterIcon fontSize='large'/>
+             </Box> 
+          
+                  
+          </Button>
+          <Button
+           href="#" 
+           color="success"
+           variant="contained"
+           style={{padding: "2rem"}}>
+             <Box>
+               <WhatsAppIcon fontSize='large'/>
+             </Box> 
+          
+           </Button>
       </Box>
     </Fragment>
   )
