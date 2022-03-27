a565d3a2db8cd06b13b20dacbab08106655a1aa8
THOMASGREG\germane.orozco <germane.orozco@thomasgreg.com>
2022-03-27 08:34:30 -0500

    Se soluciona la muestra del logo en el about y el tamaño para que sea responsive.

--- source/NumenProyect/src/views/About.jsx
+++ source/NumenProyect/src/views/About.jsx
@@ -11,12 +11,22 @@ const About = () => {
       >
         <Card>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={5} md={5} lg={5}>
-              <i
+            <Grid
+              item
+              xs={12}
+              sm={5}
+              md={5}
+              lg={5}
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            >
+              <img
                 //className="fas fa-store"
-                img src='https://res.cloudinary.com/jimefioni/image/upload/v1648253242/images%20Numen/imagenes/0BD9DF75-EAC0-437A-95CE-AD874416B74E_foxdry.jpg'
-                style={{ fontSize: '200px', padding: '10px'}}
-              ></i>
+                src="https://res.cloudinary.com/jimefioni/image/upload/v1648253242/images%20Numen/imagenes/0BD9DF75-EAC0-437A-95CE-AD874416B74E_foxdry.jpg"
+                style={{
+                  padding: '10px',
+                }}
+                width="75%"
+              ></img>
             </Grid>
             <Grid item xs={12} sm={7} md={7} lg={7}>
               <Typography variant="h4">Mundo Animal</Typography>
@@ -28,7 +38,14 @@ const About = () => {
                   whiteSpace: 'pre-wrap',
                 }}
               >
-                Somos una tienda para mascotas, creada en Octubre de 2020, con el propósito de ser un lugar ameno y donde se pueda encontrar todo tipo de alimento balanceado para perros y gatos. Accesorios de todo tipo, incluyendo: ropa, collares, correas, cuchas,transportadores, etc. La predisposición es uno de los pilares fundamentales en nuestra gente, la dedicación especial a cada cliente, proponiendo así el logro de una mejora calidad de vida de cada mascota que llega. {' '}
+                Somos una tienda para mascotas, creada en Octubre de 2020, con
+                el propósito de ser un lugar ameno y donde se pueda encontrar
+                todo tipo de alimento balanceado para perros y gatos. Accesorios
+                de todo tipo, incluyendo: ropa, collares, correas,
+                cuchas,transportadores, etc. La predisposición es uno de los
+                pilares fundamentales en nuestra gente, la dedicación especial a
+                cada cliente, proponiendo así el logro de una mejora calidad de
+                vida de cada mascota que llega.{' '}
               </Typography>
             </Grid>
           </Grid>
