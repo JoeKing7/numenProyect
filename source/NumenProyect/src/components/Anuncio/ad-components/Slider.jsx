e7599b049142d58385413882deaaf84329184fe3
HernanGBV91 <90531768+HernanGBV91@users.noreply.github.com>
2022-03-27 11:28:36 -0300

    Se ajustan titulos en la sección "marcas" del db

--- source/NumenProyect/src/components/Anuncio/ad-components/Slider.jsx
+++ source/NumenProyect/src/components/Anuncio/ad-components/Slider.jsx
@@ -13,25 +13,29 @@ import 'swiper/css/scrollbar';
 
 export const Slider = () => {
   // llamado al back 
-  return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      >
-
-      <SwiperSlide> Dogui </SwiperSlide>
-      <SwiperSlide> Insignia </SwiperSlide>
-      <SwiperSlide> Excellent </SwiperSlide>
-      <SwiperSlide> Royal Canin </SwiperSlide>
-      <SwiperSlide> Vagoneta </SwiperSlide>
-    </Swiper>
-  );
-};
+      return (
+        <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        >
+        {json.map( marcas => (
+          <SwiperSlide key={marcas.id}>
+            <div className="slide-content">
+              <h5>{marcas.marca}</h5>
+              <div className="marca-image">
+                <img src={marcas.fotomarca} alt="" className="marca-foto"></img>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  };
 
